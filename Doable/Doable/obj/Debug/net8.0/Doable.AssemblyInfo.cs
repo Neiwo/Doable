7d1bc8946dd55041a3b2dcdd045b78c5a873ddec
owen yap <bc.johnowen.yap@cvsu.edu.ph>
2024-05-28 01:11:38 +0800

    task and db

--- Doable/Doable/obj/Debug/net8.0/Doable.AssemblyInfo.cs
+++ Doable/Doable/obj/Debug/net8.0/Doable.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Doable")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab954a14ffe19bc724ebb8da527e2e62974a0264")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8855b27572abf618b93fa41c0f8ea36f43825327")]
 [assembly: System.Reflection.AssemblyProductAttribute("Doable")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Doable")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
